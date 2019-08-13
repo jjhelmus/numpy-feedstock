7bb71ecbb51beceb47227f4d2956b7d3d98a0f7c
Sophia Castellarin <scastellarin@anaconda.com>
2019-08-13 16:34:33 -0500

    Save point

--- recipe/install_base.sh
+++ recipe/install_base.sh
@@ -7,7 +7,7 @@ cp $RECIPE_DIR/test_fft.py numpy/fft/tests
 
 # site.cfg should not be defined here.  It is provided by blas devel packages (either mkl-devel or openblas-devel)
 # Urgh ..
-export CFLAGS="${CFLAGS} -Wno-implicit-fallthrough -Wno-unused-parameter -Wno-missing-field-initializers"
+export CFLAGS="${CFLAGS} -I$PREFIX/include -Wno-implicit-fallthrough -Wno-unused-parameter -Wno-missing-field-initializers"
 if [[ ${target_platform} == osx-64 ]]; then
     export LDFLAGS="$LDFLAGS -undefined dynamic_lookup"
 else
@@ -20,5 +20,5 @@ if [[ ${DEBUG_PY} == yes ]]; then
 fi
 
 ${PYTHON} setup.py config
-${PYTHON} setup.py build ${DBG}
+${PYTHON} setup.py build --debug -j 1
 ${PYTHON} setup.py install --single-version-externally-managed --record=record.txt
