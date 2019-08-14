a4a63312ba64408db29a82411d09a38725d17445
Jonathan Helmus <jjhelmus@gmail.com>
2019-08-14 11:59:14 -0500

    WIP: working build

--- recipe/install_base.sh
+++ recipe/install_base.sh
@@ -3,22 +3,22 @@
 set -e
 
 cp $PREFIX/site.cfg site.cfg
-cp $RECIPE_DIR/test_fft.py numpy/fft/tests
+#cp $RECIPE_DIR/test_fft.py numpy/fft/tests
 
 # site.cfg should not be defined here.  It is provided by blas devel packages (either mkl-devel or openblas-devel)
 # Urgh ..
-export CFLAGS="${CFLAGS} -I$PREFIX/include -Wno-implicit-fallthrough -Wno-unused-parameter -Wno-missing-field-initializers"
-if [[ ${target_platform} == osx-64 ]]; then
-    export LDFLAGS="$LDFLAGS -undefined dynamic_lookup"
-else
-    export LDFLAGS="$LDFLAGS -shared"
-    export FFLAGS="$FFLAGS -Wl,-shared"
-fi
-
-if [[ ${DEBUG_PY} == yes ]]; then
-  DBG="--debug"
-fi
+#export CFLAGS="${CFLAGS} -I$PREFIX/include -Wno-implicit-fallthrough -Wno-unused-parameter -Wno-missing-field-initializers"
+#if [[ ${target_platform} == osx-64 ]]; then
+#    export LDFLAGS="$LDFLAGS -undefined dynamic_lookup"
+#else
+#    export LDFLAGS="$LDFLAGS -shared"
+#    export FFLAGS="$FFLAGS -Wl,-shared"
+#fi
+#
+#if [[ ${DEBUG_PY} == yes ]]; then
+#  DBG="--debug"
+#fi
 
 ${PYTHON} setup.py config
-${PYTHON} setup.py build --debug -j 1
+${PYTHON} setup.py build -j 32
 ${PYTHON} setup.py install --single-version-externally-managed --record=record.txt
