115a06183469b9378059564937a83a5d6bfb747c
Jonathan Helmus <jjhelmus@gmail.com>
2020-07-30 14:08:12 -0500

    clean up install_base.sh, recipe and tests

--- recipe/install_base.sh
+++ recipe/install_base.sh
@@ -2,24 +2,7 @@
 
 set -e
 
+# site.cfg is provided by blas devel packages (either mkl-devel or openblas-devel)
 cp $PREFIX/site.cfg site.cfg
-cp $RECIPE_DIR/test_fft.py numpy/fft/tests
-
-# site.cfg should not be defined here.  It is provided by blas devel packages (either mkl-devel or openblas-devel)
-# Urgh ..
-export CFLAGS="${CFLAGS} -Wno-implicit-fallthrough -Wno-unused-parameter -Wno-missing-field-initializers"
-if [[ ${target_platform} == osx-64 ]]; then
-    export LDFLAGS="$LDFLAGS -undefined dynamic_lookup"
-    # as of python 3.7.4, CFLAGS env variable as well as sysconfig flags are both used
-    # this prevents failures in the numpy long_double tests
-    export CFLAGS="$CFLAGS -fno-lto"
-else
-    export LDFLAGS="$LDFLAGS -shared"
-    export FFLAGS="$FFLAGS -Wl,-shared"
-fi
-
-if [[ ${DEBUG_PY} == yes ]]; then
-  DBG="--debug"
-fi
 
 ${PYTHON} -m pip install --no-deps --ignore-installed -v .
