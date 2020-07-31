a6fb15d9afeb823e4654f08f8c9ef3af84df32ac
Jonathan J. Helmus <jjhelmus@gmail.com>
2020-07-31 11:51:45 -0700

    add entry point on windows

--- recipe/install_base.bat
+++ recipe/install_base.bat
@@ -5,4 +5,8 @@ COPY %PREFIX%\site.cfg site.cfg
 %PYTHON% -m pip install --no-deps --ignore-installed -v .
 if errorlevel 1 exit 1
 
-COPY %RECIPE_DIR%\f2py.bat %PREFIX%\Scripts
+XCOPY %RECIPE_DIR%\f2py.bat %SCRIPTS% /s /e
+if errorlevel 1 exit 1
+
+del %SCRIPTS%\f2py.exe
+if errorlevel 1 exit 1
